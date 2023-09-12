552448bf7b161913cdcbe08253313bf061ae6203
chichoon <chichoon.choi@gmail.com>
2023-09-12 14:09:11 +0900

    chore: :wrench: git hook 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
       <header className='App-header'>
@@ -8,11 +8,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
+          ㅁㅁㅁ
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
