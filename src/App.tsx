6bca704a6db83ecac3b2979770d21803a9f8624c
chichoon <chichoon.choi@gmail.com>
2023-09-12 17:43:55 +0900

    feat: :lipstick: 버튼 스타일링 및 이벤트 버블링으로 버튼 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { VictoryArea, VictoryAxis, VictoryBar, VictoryChart, VictoryTheme, VictoryTooltip } from 'victory';
 
 import { ResponseType } from 'types/data';
 import { getData } from 'services';
-import './App.css';
+
+import styles from './App.module.scss';
 
 const App = () => {
   const [data, setData] = useState<ResponseType>({});
   const [selectedId, setSelectedId] = useState<string>('');
+
   const keys = Object.keys(data).map((key) => new Date(key));
   const values = Object.values(data);
   const ids = [...new Set(values.map((value) => value.id))];
   const areaValues = values.map((value, index) => ({
     x: keys[index],
     y: value.value_area,
-    id: value.id,
   }));
   const barValues = values.map((value, index) => ({
     x: keys[index],
@@ -25,6 +26,10 @@ const App = () => {
 
   const barMaximum = Math.max(...barValues.map((value) => value.y));
   const areaMaximum = Math.max(...areaValues.map((value) => value.y));
+
+  function handleClickButtons(e: MouseEvent<HTMLButtonElement>) {
+    setSelectedId((e.target as Element).attributes.getNamedItem('data-id')?.value ?? '');
+  }
 
   useEffect(() => {
     getData().then(({ response }) => {
@@ -55,7 +60,7 @@ const App = () => {
           data={barValues}
           scale={{ x: 'time', y: 'linear' }}
           labelComponent={<VictoryTooltip />}
-          style={{ data: { width: 7, fill: ({ datum }) => (datum.id === selectedId ? '#ff0000' : '#333333') } }}
+          style={{ data: { width: 7, fill: ({ datum }) => (datum.id === selectedId ? '#87ceeb' : '#333333') } }}
           standalone={false}
         />
         <VictoryAxis
@@ -72,20 +77,20 @@ const App = () => {
           data={areaValues}
           y={(datum) => datum.y / areaMaximum / 2}
           scale={{ x: 'time', y: 'linear' }}
-          style={{ data: { fill: 'rgba(100, 250, 100)' }, border: { color: 'black' } }}
+          style={{ data: { fill: 'rgba(100, 250, 100, 0.8)' }, border: { color: 'black' } }}
           standalone={false}
         />
       </VictoryChart>
-      <select onChange={(event) => setSelectedId(event.target.value)}>
-        <option key={0} value=''>
+      <button className={styles.buttonWrapper} type='button' onClick={handleClickButtons}>
+        <button type='button' data-id='none' className={styles.buttons}>
           없음
-        </option>
+        </button>
         {ids.map((id) => (
-          <option key={id} value={id}>
+          <button type='button' data-id={id} key={id} className={styles.buttons}>
             {id}
-          </option>
+          </button>
         ))}
-      </select>
+      </button>
     </>
   );
 };
--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-import { DataType } from 'types/data';
-
-export const DUMMY_DATA: DataType = {
-  type: 'dummy',
-  version: 1,
-  response: {},
-};
